95a43a141d1542086f4f0b650d3b532796dd59af
DavidPersson <david.persson@elev.ga.ntig.se>
2019-09-12 10:28:02 +0200

    Genererat geters och seters och även toString

--- src/Message.java
+++ src/Message.java
@@ -4,40 +4,71 @@
     Message hanterings system
  */
 
-public class Message {
-    public static void main(String[] args) {
-        String author;
-        String message;
-        Date createdAt;
-        Date updatedAt;
+import java.util.Date;
 
+
+public class Message {
+    public String author;
+    public String message;
+    public Date createdAt;
+    public Date updatedAt;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Message(String message){ //metoden för meddelandet
+        this.message = message;
+        this.author = System.getProperty("Username");
+        Date createdAt = new Date();
+        Date updatedAt = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
 
         if  (args.length == 0) //Om inga argument anges så printar programmet bara menyn
-            System.out.println(" Show message 1 \n" +
-                    " Add message 2 \n" +
-                    " Update message 3 \n" +
-                    " Save message to file 4 \n" +
-                    " Upload message from file 5 \n" +
-                    " Exit 6");
-        for (int i = 0 ; i < args.length ; i++)
-            if (args.length == 1) { //Om argument 1 anges
-                System.out.println(" Existing messages ");
-        }
-        if (args.length == 2) { //Om argument 2 anges
-            System.out.println(" Add message ");
-        }
-        if (args.length == 3) { //Om argument 3 anges
-            System.out.println(" Update message ");
-        }
-        if (args.length == 4) { //Om argument 4 anges
-            System.out.println(" Save message as file ");
-        }
-        if (args.length == 5) { //Om argument 5 anges
-            System.out.println(" Open message as file ");
-        }
-        if (args.length == 6) { //Om argument 6 anges
-            System.out.println(" Exiting ");
-        }
+            System.out.println(
+                " Show message \n" +
+                " Add message \n" +
+                " Update message \n" +
+                " Save message to file \n" +
+                " Upload message from file \n" +
+                " Exit");
 
     }
 }
